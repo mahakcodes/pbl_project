c936a82c85745e2a0c58d9118cc74e5aef6f9548
Ishita Kuntal <131ishita@gmail.com>
2026-04-09 23:17:15 +0530

    add teacher api

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
-from .views import LoginView
-urlpatterns = [path('login/', LoginView.as_view(), name='login')]
+from .views import LoginView, create_teacher
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('create-teacher/', create_teacher),  # 👈 ye add kiya
+]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,11 +1,42 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from .serializers import LoginSerializer
+
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view
+
+User = get_user_model()
+
+
 class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
     permission_classes = [permissions.AllowAny]
+
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         u = serializer.validated_data['user']
-        return Response({'id': u.id, 'username': u.username, 'email': u.email, 'role': u.role, 'roll_number': u.roll_number, 'employee_id': u.employee_id, 'department': u.department, 'first_name': u.first_name, 'last_name': u.last_name})
+        return Response({
+            'id': u.id,
+            'username': u.username,
+            'email': u.email,
+            'role': u.role,
+            'roll_number': u.roll_number,
+            'employee_id': u.employee_id,
+            'department': u.department,
+            'first_name': u.first_name,
+            'last_name': u.last_name
+        })
+
+
+@api_view(['GET'])
+def create_teacher(request):
+    if not User.objects.filter(username="teacher").exists():
+        User.objects.create_user(
+            username="teacher",
+            email="teacher@college.edu",
+            password="1234",
+            role="teacher"
+        )
+        return Response({"msg": "Teacher created"})
+    return Response({"msg": "Teacher already exists"})
