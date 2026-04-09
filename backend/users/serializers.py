22b423f8f09d8b5cb3910db1bb198dabcac6d11f
Ishita Kuntal <131ishita@gmail.com>
2026-04-10 00:04:19 +0530

    final fix login

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -3,22 +3,27 @@ from rest_framework import serializers
 
 User = get_user_model()
 
+
 class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    email = serializers.CharField()
     password = serializers.CharField()
 
     def validate(self, data):
         email = data.get("email")
         password = data.get("password")
 
+        # user find by email
         user_obj = User.objects.filter(email=email).first()
 
         if user_obj:
-            user = authenticate(username=user_obj.username, password=password)
+            user = authenticate(
+                username=user_obj.username,
+                password=password
+            )
         else:
             user = None
 
-        if not user:
+        if user is None:
             raise serializers.ValidationError("Invalid credentials")
 
         return {"user": user}
