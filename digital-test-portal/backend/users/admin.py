0e88d4322cfffc08f5caf2544217a8e7c509998d
Mahak Maheshwari <mahak.maheshwari06@gmail.com>
2026-04-12 12:50:31 +0530

    Delete digital-test-portal directory

--- digital-test-portal/backend/users/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User, StudentProfile, TeacherProfile
-
-admin.site.register(User)
-admin.site.register(StudentProfile)
-admin.site.register(TeacherProfile)
